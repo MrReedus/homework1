8132eb5929472087d9f5d0ffeb7809aaba47e5df
Evgenii <naterid@yandex.ru>
2023-07-01 14:09:01 +0700

    finished functional of filtered

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -8,21 +8,16 @@ import s2 from '../../s1-main/App.module.css'
 * 3 - дописать типы и логику функции filterAffairs и проверить её тестами
 * 4 - выполнить пункт 3 для функции deleteAffair
 * 5 - указать нужный тип в useState с affairs
-*
 * 6 - дописать тип и логику функции deleteAffairCallback
-*
 * 7 - в файле Affairs.tsx дописать типизацию пропсов
-*
 * 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
-*
 * 9 - в файле Affair.tsx дописать типизацию пропсов
-*
 * 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
 * 11 - в файле Affair.tsx отобразить приходящие данные !
 * */
 
 // types
-export type AffairPriorityType = 'high' | 'low' | string  // need to fix any
+export type AffairPriorityType = 'high' | 'low' | 'middle'  // need to fix any
 export type AffairType = {
     _id: number // need to fix any
     name: string // need to fix any
